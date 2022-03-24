bf3efd0a4b5ab9d721fb7592ca75940794d5610d
Chris Salois <csalois@gmail.com>
2022-03-24 11:45:17 -0400

    commit tree 7e9135e87e27c1b2a61aee50f2b07bb73248946c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const left = parseInt(dodger.style.left);
+  if (left > 0) dodger.style.left = `${left - 1}px`;
+}
+
+function moveDodgerRight() {
+  const left = parseInt(dodger.style.left);
+  if (left < 360) dodger.style.left = `${left + 1}px`;
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowLeft') moveDodgerLeft();
+  if (e.key === 'ArrowRight') moveDodgerRight();
+})
